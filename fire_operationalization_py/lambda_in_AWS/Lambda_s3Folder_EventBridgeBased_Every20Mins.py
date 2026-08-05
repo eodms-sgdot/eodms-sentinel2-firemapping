e3d40cce47052d92e9986912a5f2b6a6fc84d53b
Kumari Gurusamy <kumari.gurusamy@nrcan-rncan.gc.ca>
2026-08-05 14:30:31 -0400

    update to lambda code
    Both yesterday and today are to be used in prefix since some files get dropped later the next day morning

--- fire_operationalization_py/lambda_in_AWS/Lambda_s3Folder_EventBridgeBased_Every20Mins.py
+++ fire_operationalization_py/lambda_in_AWS/Lambda_s3Folder_EventBridgeBased_Every20Mins.py
@@ -42,19 +42,30 @@ def lambda_handler(event, context):
     print(f"context: {context}")
     now = datetime.now(timezone.utc)
     s3_folder_today = now.strftime("Sentinel-2/S2MSI2A/%Y/%m/%d/")
+    #since yesterdays data can be loaded upto today morning, we need to have two days in prefix
+    today = datetime.now(timezone.utc)
+    yesterday = today - timedelta(days=1)
+    s3_folder_yesterday = yesterday.strftime("Sentinel-2/S2MSI2A/%Y/%m/%d/")
+    print(f"Is it catching yesterday correctly? {yesterday} and {s3_folder_yesterday}")
+
     #bucket_name and s3_folder can be sent from test
     bucket_name = event.get('bucket_name', "sentinel-products-ca-mirror")
-    s3_folder = event.get('s3_folder', s3_folder_today)
-    
-    #if you want from 2 days ago, write manual date here, if it has been cloudy today
-    #s3_folder= "Sentinel-2/S2MSI2A/2026/07/23/"
+    s3_folder_today = event.get('s3_folder_today', s3_folder_today)
+    s3_folder_yesterday = event.get('s3_folder_yesterday', s3_folder_yesterday)
+
+    prefixes = [
+        s3_folder_today,
+        s3_folder_yesterday
+        ]
+    #this way we will pick up August 4th data that got loaded on August 5th morning
     #Modify this to a proper project name, and find files in that folder
     fireProjectName = "Lambda_Function_s3Folder_ActiveFireRecord_Test"
     bands = ["B12"]
-    
-    activeFireRecords = (f"Process_Records/{fireProjectName}_%Y_%m_%d_s3Folder_activeFireRecords.csv")
-    activeFireFileName= now.strftime(activeFireRecords)
-    activeFireTools.start_a_new_activeFire_csv_record(activeFireFileName)
+    # We shouldnt a create a new csv for every lambda run, we should append records to the same one!
+    activeFireRecords = (f"Process_Records/{fireProjectName}_2026_08_05_s3Folder_activeFireRecords.csv")
+    #activeFireRecords = (f"Process_Records/{fireProjectName}_%Y_%m_%d_s3Folder_activeFireRecords.csv")
+    #activeFireFileName= now.strftime(activeFireRecords)
+    #activeFireTools.start_a_new_activeFire_csv_record(activeFireFileName)
     #--------------------------------------------------------------------------
     # Here we set the start time and end time for searching the AWS for zip
     #-----------------------------------------------------------------------------
@@ -62,80 +73,86 @@ def lambda_handler(event, context):
     start_time = datetime.now(timezone.utc)
     last_check = start_time - timedelta(minutes=20)
     INTERVAL = 20 * 60 # 20 minutes #to decide how many seconds to sleep after process complets
-    """        
-    #if you want from 2 days ago, since it has been cloudy
-    target_day = (datetime.now(timezone.utc) - timedelta(days=8)).date()
-    #if you want between two different days
-    start_time = datetime.combine(target_day, datetime.min.time(), tzinfo=timezone.utc)
-    end_time = datetime.combine(target_day, datetime.max.time(), tzinfo=timezone.utc)
-    """
     print("Time is: ", datetime.now(timezone.utc))
     print("Last check is 20 minutes ago: ", last_check)
-    prefix = s3_folder.lstrip('/')
-    print("Checking in folder: ", prefix)
+
     filtered_links = [] # to filter for MSIL2A for example, so we can use specific products and not all of them 
     try:
-        while True:
+        objects_today_yesterday = []
+        print("--------- contents for the today --------------------")
+        for prefix in prefixes:
+            print(f"Checking for {prefix}")
             response = s3.list_objects_v2(
                 Bucket=bucket_name,
                 Prefix=prefix,
-                MaxKeys=100 # assuming only 100 gets dumped every 5 minutes
-            )
-            #-------------------------------------------------------------------------------------------------------------------------------------------------------
-            #If you dont see any data, most likely the prefix date has to be changed - try yesterday's date if its morning
-            #-------------------------------------------------------------------------------------------------------------------------------------------------------
+                MaxKeys=1000) # in a day there is usually 600 - 800 images, we will process only the last 20 minutes ones)
+            num_objects = len(response.get("Contents", []))
+            print("Number of Objects in ", prefix, ": ", num_objects)
             if "Contents" in response:
-                count = 0
-                print("Only images with active fire values - detected with SWIR > 10000 will be used to create fire map and recorded in a csv for reference")
-                #starting to process zip file to see if there is active fire
-                numberOfFilesProcessed = 0
-                for obj in response["Contents"]:
-                    product_link = f"https://sentinel-products-ca-mirror.s3.ca-central-1.amazonaws.com/{obj['Key']}"
-                    print("Product Link:", product_link)  
-                    cog_format = {} # to store all bands in a product in a list of lists, one by one, here, and a list in Jupyter lab.
-                    if obj["LastModified"] > last_check and "MSIL2A" in product_link:
-                    #if "MSIL2A" in product_link:
-                        count = count + 1
-                        ProcessDateTime = datetime.now()
-                        S2FileName = product_link.split("/Sentinel-2/")[1]
-                        # these asterisks help separate the log for each file processed
-                        print("**************************************************************************************************")
-                        print(count, ": New file:", {obj['Key']})
+                objects_today_yesterday.extend(response.get("Contents", []))
+                print("IsTruncated, if more than 1000:", response.get("IsTruncated"))  # True if more objects exist beyond MaxKeys
+                print("--------- contents for the yesterday --------------------")
+        print(f"Total number of objects for today and  yesterday: {len(objects_today_yesterday)}")
+        if len(objects_today_yesterday) > 0:
+            recent_objects = [
+                obj
+                for obj in objects_today_yesterday
+                if obj["LastModified"] >= last_check]            
+            print(f"Found {len(recent_objects)} recent objects")
+            count = 0
+            print("Purpose: Images with active fire values - detected with SWIR > 10000 will be used to create fire map and recorded in a csv for reference")
+            print(f"Files processed: Images loaded between {last_check} and {start_time}")
+            #starting to process zip file to see if there is active fire
+            numberOfFilesProcessed = 0
+            #for obj in response["Contents"]: dont have to process all 1000
+            for obj in recent_objects:
+                cog_format = {} # to store all bands in a product in a list of lists, one by one, here, and a list in Jupyter lab.
+                product_link = f"https://sentinel-products-ca-mirror.s3.ca-central-1.amazonaws.com/{obj['Key']}"
+                #if obj["LastModified"] > last_check and "MSIL2A" in product_link:
+                if "MSIL2A" in product_link:
+                    count = count + 1
+                    #print("Product Link:", product_link)  
+                    ProcessDateTime = datetime.now()
+                    S2FileName = product_link.split("/Sentinel-2/")[1]
+                    # these asterisks help separate the log for each file processed
+                    print("**************************************************************************************************")
+                    print(count, ": New file:", {obj['Key']})
 
-                        inputDataDir, cloudPercent, crs = activeFireTools.process_product_link(product_link, fireProjectName)
-                        safeFileName_withSAFE_Ext = os.path.basename(inputDataDir)
-                        safeFileName = os.path.splitext(safeFileName_withSAFE_Ext)[0]
+                    inputDataDir, cloudPercent, crs = activeFireTools.process_product_link(product_link, fireProjectName)
+                    safeFileName_withSAFE_Ext = os.path.basename(inputDataDir)
+                    safeFileName = os.path.splitext(safeFileName_withSAFE_Ext)[0]
 
-                        print(f"Starting to create COGS for only B12 bands in {inputDataDir} to detect active Fire")
-                        print(f"Safe file name is: {safeFileName}")
-                        cog_format[safeFileName] = activeFireTools.get_bands_as_cogs(bands,inputDataDir, fireProjectName)     
-                        print(f"Done with storing inputs as COGs, so deleting {inputDataDir} having completed creating maps (cloud = {cloudPercent}%)")
-                        shutil.rmtree(inputDataDir)
-                        #---------------------------------------------------------------------------------------------------------------------------------------
-                        for product, cog_list in cog_format.items():
-                            print(f"Product is {product}")
-                            
-                            B12_cog = cog_list.get("B12")
-                            activeFirePixels = activeFireTools.findIfActiveFire(B12_cog)
+                    print(f"Starting to create COGS for only B12 bands in {inputDataDir} to detect active Fire")
+                    print(f"Safe file name is: {safeFileName}")
+                    cog_format[safeFileName] = activeFireTools.get_bands_as_cogs(bands,inputDataDir, fireProjectName)     
+                    print(f"Done with storing inputs as COGs, so deleting {inputDataDir} having completed creating maps (cloud = {cloudPercent}%)")
+                    shutil.rmtree(inputDataDir)
+                    #---------------------------------------------------------------------------------------------------------------------------------------
+                    for product, cog_list in cog_format.items():
+                        print(f"Product is {product}")
+                        
+                        B12_cog = cog_list.get("B12")
+                        activeFirePixels = activeFireTools.findIfActiveFire(B12_cog)
 
-                            if (activeFirePixels > 0): 
-                                numberOfFilesProcessed = numberOfFilesProcessed + 1
-                                ProcessDateTime = datetime.now()
-                                S2FileName = product
-                                Decision = "check fire composites"
-                                table_data = [ProcessDateTime, fireProjectName, product, cloudPercent, activeFirePixels, Decision]
-                                activeFireTools.write_to_csv(activeFireFileName, table_data)
-                                print(f"Number of files with active fire that were processed to create fire composites are: {numberOfFilesProcessed}")
-                            else:
-                                inputCogPath = "/tmp/Result_Images/Input_COG_Images/" + fireProjectName + "/" + product 
-                                print(f"Deleting input COG {inputCogPath} because number of active fire pixels was only {activeFirePixels} ")
-                                shutil.rmtree(inputCogPath)
-            else:
-                print(f"Failed to retrieve features. {response.status_code}")
-            # if the process takes eight minutes, it will sleep 12 minutes - for every 20 minute check
-            elapsed = (datetime.now(timezone.utc) - start_time).total_seconds()
-            sleep_time = max(0, INTERVAL - elapsed)
-            time.sleep(sleep_time)
+                        if (activeFirePixels > 10): 
+                            numberOfFilesProcessed = numberOfFilesProcessed + 1
+                            ProcessDateTime = datetime.now()
+                            S2FileName = product
+                            Decision = "check fire composites"
+                            table_data = [ProcessDateTime, fireProjectName, product, cloudPercent, activeFirePixels, Decision]
+                            activeFireTools.write_to_csv(activeFireFileName, table_data)
+                            print(f"Number of files with active fire that were processed to create fire composites are: {numberOfFilesProcessed}")
+                        else:
+                            inputCogPath = "/tmp/Result_Images/Input_COG_Images/" + fireProjectName + "/" + product 
+                            print(f"Deleting input COG {inputCogPath} because number of active fire pixels was only {activeFirePixels} ")
+                            shutil.rmtree(inputCogPath)
+        else:
+            print(f"Failed to retrieve features. {response.status_code}")
+        # if the process takes eight minutes, it will sleep 12 minutes - for every 20 minute check
+        elapsed = (datetime.now(timezone.utc) - start_time).total_seconds()
+        print(f"Time taken to complete the process {elapsed} seconds")
+        #sleep_time = max(0, INTERVAL - elapsed)
+        #time.sleep(sleep_time) # no point to this, since in 15 minutes lambda will end - keeping it any way
     except Exception:
         trc_back = traceback.format_exc()
         return {'event': event, 'Error': trc_back}
---
+++ fire_operationalization_py/lambda_in_AWS/activeFire_FilterTools.py
@@ -0,0 +1,250 @@
+import os
+import requests
+import io
+import zipfile
+
+import rasterio
+from rasterio.shutil import copy as rio_copy
+from rasterio.enums import Resampling
+
+import boto3
+from io import StringIO
+import csv 
+# for cloud level records
+import xml.etree.ElementTree as ET
+import glob
+
+#for counting active fire pixels
+import numpy as np
+
+s3 = boto3.client("s3")
+bucket_name = "s2-fire-ard"
+
+def start_a_new_activeFire_csv_record(resDataFileName):
+    s3_key = resDataFileName
+    csv_buffer = StringIO() #RAM buffer
+    writer = csv.writer(csv_buffer)
+    writer.writerow([
+    'ProcessDateTime',
+    'FireProjectName',
+    'S2FileName',
+    'CloudPercent',
+    'activeFirePixels',
+    'Decision'
+    ])
+
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=s3_key,
+        Body=csv_buffer.getvalue(),
+        ContentType='text/csv'
+        )
+    print(f"CSV created in s3://{bucket_name}/{s3_key}")   
+
+def write_to_csv(filename, data):
+    key = filename
+
+    # Read existing CSV
+    obj = s3.get_object(Bucket=bucket_name, Key=key)
+    existing_csv = obj['Body'].read().decode('utf-8')
+
+    # Load into buffer
+    buffer = StringIO(existing_csv)
+
+    # Move cursor to end
+    buffer.seek(0, 2)
+
+    writer = csv.writer(buffer)
+
+    writer.writerow(data)
+    # Upload updated CSV
+    s3.put_object(
+    Bucket=bucket_name,
+    Key=key,
+    Body=buffer.getvalue(),
+    ContentType='text/csv'
+    )
+# ---------------------------------------------
+# Function to download the s3 data locally to process
+# ---------------------------------------------
+def process_product_link(product_link, firename):
+    zip_url = product_link
+    # Directory to extract files to
+    extract_to = f"/tmp/sentinel_2_data/2026_Fires/{firename}"
+    # Download and extract a sentinel 2 SAFE zip
+    os.makedirs(extract_to, exist_ok=True)
+
+    # Extract all files
+    zip_name = os.path.splitext(os.path.basename(zip_url))[0]
+    # Download and extract
+    response = requests.get(zip_url)
+    if response.status_code == 200:
+        with zipfile.ZipFile(io.BytesIO(response.content)) as zip_ref:
+            zip_ref.extractall(extract_to)
+        start_directory = extract_to + "/" + zip_name + ".SAFE"
+        print(f"-------- Files extracted to '{start_directory}' ")
+        crs, cloud_level = find_cloud_level(start_directory)
+        return start_directory, cloud_level, crs
+    else:
+        print(f"Failed to download ZIP file. Status code: {response.status_code}")
+        return none
+
+# ---------------------------------------------
+# Function to find the image data directory in the .SAFE folder
+# Called by get_bands_as_cogs(...)
+# Returns img_data path
+# ---------------------------------------------
+def find_img_data_dir(start_path):
+    for root, dirs, files in os.walk(start_path):
+        if "IMG_DATA" in dirs:
+            return os.path.join(root, "IMG_DATA")
+    return None
+
+# ---------------------------------------------
+# Function to find the specific band inside image data directory in the .SAFE folder
+# Called by find_band_jp2file_make_cog(..)
+# Returns band_file_root_directory and band_file
+# ---------------------------------------------
+def find_file_by_band_name(search_dir, band_to_search):
+    for root, dirs, files in os.walk(search_dir):
+        for file in files:
+            if band_to_search in file:
+                #return os.path.join(root, file)
+                return root, file
+    return None
+
+# ---------------------------------------------
+# Function to find the band in the image data directory and make .jp2 into .cog
+# Called by get_bands_as_cogs(...)
+# Returns .cog of the band_file
+# ---------------------------------------------
+def find_band_jp2file_make_cog(directory_to_search, band_to_search, safeFileName, firename):
+    band_file_dir, found_file = find_file_by_band_name(directory_to_search, band_to_search)
+    band_filename = os.path.splitext(os.path.basename(found_file))[0]
+    if found_file:
+        print(f"File found: {found_file}")
+        print(f"Base name of file: {band_filename}")
+        print(f"Directory of file: {band_file_dir}")
+    else:
+        print("No matching file found.")
+     # Input JP2 file path
+    input_jp2 = band_file_dir + "/" + band_filename + ".jp2"
+    
+    #with rasterio.open(input_jp2) as src:
+        #jp2_crs = src.crs
+    
+     # Output COG file path
+    output_dir = (
+    f"/tmp/Result_Images/Input_COG_Images/{firename}/{safeFileName}")
+    # Create folders if they don't exist
+    os.makedirs(output_dir, exist_ok=True)
+
+    local_output_cog = os.path.join(
+        output_dir,
+        band_filename + "_cog.tif"
+    )
+
+    # Convert JP2 to COG
+    rio_copy(
+        input_jp2, 
+        local_output_cog, 
+        driver = 'COG',
+        compress = 'deflate',
+        blocksize = 512,
+        overview_resampling = Resampling.nearest
+        )
+    print(f"Converted {input_jp2} to Cloud Optimized GeoTIFF: {local_output_cog}")
+    numberOfActiveFirePixels = findIfActiveFire(local_output_cog)
+    if numberOfActiveFirePixels > 10:
+        print("Saving COG to S3 for analysis because there is more than (10 pixels) 100 sq.m of active fire")    
+        s3.upload_file(
+            local_output_cog,
+            "s2-fire-ard",
+            f"Result_Images/Input_COG_Images/{firename}/{safeFileName}/{band_filename}_cog.tif"
+            )
+    return local_output_cog
+
+# ---------------------------------------------
+# Function to return a list of cog band files after converting .jp2 in SAFE into .cog
+# Called by watch_function (...)
+# Returns a list of .cog of the bands
+# ---------------------------------------------        
+def get_bands_as_cogs(bands, inputDataDir, firename):
+    img_data_path = find_img_data_dir(inputDataDir)
+    safeFileName_withSAFE_Ext = os.path.basename(inputDataDir)
+    safeFileName = os.path.splitext(safeFileName_withSAFE_Ext)[0]
+    
+    if img_data_path:
+        print(f"'img_data' directory found at: {img_data_path}")
+    else:
+        print("No 'img_data' directory found.")
+    
+    results = {}
+    
+    for band in bands:
+        if band == "B04":
+            path = img_data_path + "/R20m"
+        else:
+            path = img_data_path
+    
+        results[band] = find_band_jp2file_make_cog(path, band, safeFileName, firename)
+        
+    return results
+    
+   
+# ---------------------------------------------
+# Function to find cloud percentage to decide whether to create firemap
+# ---------------------------------------------
+def find_cloud_level(start_directory):
+    
+    xml_file = None
+
+    for root, dirs, files in os.walk(start_directory):
+        for file in files:
+            if file in ("MTD_MSIL2A.xml", "MTD_MSIL1C.xml"):
+                xml_file = os.path.join(root, file)
+                break
+        if xml_file: #you get inspire.xml in this not useful - check above logic
+            break
+    #"MTD_MSIL2A.xml"
+    print(f"-------- Extracting cloud level info from {xml_file}")
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    cloud = float(root.find(".//Cloud_Coverage_Assessment").text)
+    
+    # --- 1. Find product XML (cloud info) ---
+    tile_xml = glob.glob(os.path.join(start_directory, "**", "MTD_TL.xml"), recursive=True)
+
+    crs = None
+    if tile_xml:
+        tree = ET.parse(tile_xml[0])
+        root = tree.getroot()
+        #crs = root.find(".//HORIZONTAL_CS_CODE").text
+        
+        # handle namespace
+        ns = {'n1': root.tag.split('}')[0].strip('{')}
+
+        node = root.find(".//n1:HORIZONTAL_CS_CODE", ns)
+        if node is None:  # fallback if namespace not required
+            node = root.find(".//HORIZONTAL_CS_CODE")
+
+        if node is not None:
+            crs = node.text   # e.g. "EPSG:32632"
+        
+    #print (f"-------- CRS of this image is: {crs}")
+    if cloud < 30:
+        print("-------- ✅ Cloud less than 30%")
+    else:
+        print("-------- ❌ Too cloudy")
+    return crs,cloud
+    
+def findIfActiveFire(SWIR_band_12):
+    threshold = 10000 #expecting that there is active fire above this value
+    with rasterio.open(SWIR_band_12) as src:
+        data = src.read(1, masked=True)       
+        countOfFirePixels = np.count_nonzero(data > threshold)
+        if countOfFirePixels > 10: 
+             print(f"Active Fire Detected in more than 100 sq.m: Pixels above {threshold}: {countOfFirePixels}")
+        else:
+            print(f"No active fire greater than 100 sq.m in this image. Count of fire pixels: {countOfFirePixels}")
+        return countOfFirePixels
